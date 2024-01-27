81a7a7914e8a4cbba9b1549827b18ca26b54bf2b
Lucas Amaral <lucasamaralfaria@gmail.com>
2024-01-27 14:39:55 -0300

    criacao script-ORM para db

--- to_do_list/list/admin.py
+++ to_do_list/list/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from list.models import (
+    List,
+    ListItem
+)
 
-# Register your models here.
+admin.site.register(List)
+admin.site.register(ListItem)
--- to_do_list/user/models.py
+++ to_do_list/user/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
 
-# Create your models here.
+class User(AbstractBaseUser):
+    email = models.EmailField(unique=True)
+    username = models.CharField(max_length=30, unique=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    def __str__(self):
+        return self.email
