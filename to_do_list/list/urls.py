37cab23b6e03641c6c697b6134655281150dd908
Lucas Amaral <lucasamaralfaria@gmail.com>
2024-01-27 21:07:06 -0300

    endpoint get single list

--- to_do_list/list/urls.py
+++ to_do_list/list/urls.py
@@ -3,4 +3,5 @@ from list import views
 
 urlpatterns = [
     path('user_lists/', views.ListViewSet.as_view({'get':'get_user_lists'}), name='get_lists'),
+    path('get_list/<int:list_id>', views.ListViewSet.as_view({'get':'get_user_list'}),name='get user list')
 ]
--- to_do_list/list/views.py
+++ to_do_list/list/views.py
@@ -20,7 +20,16 @@ class ListViewSet(mixins.RetrieveModelMixin, mixins.CreateModelMixin, viewsets.G
         return JsonResponse({
             "lists":serializer.data
         }, status=status.HTTP_200_OK)
+    
+    def get_user_list(self,request,list_id):
+        try:
+            user_list = List.objects.get(user=request.user,id=list_id)
+        except List.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
 
+        
+        serializer = ListSerializer(user_list)
+        return Response(serializer.data)
 
 
 
