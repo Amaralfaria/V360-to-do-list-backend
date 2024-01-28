76a9ea7502a94908507b41faeaf0952b634c7c1b
Lucas Amaral <lucasamaralfaria@gmail.com>
2024-01-28 14:06:26 -0300

    Revert "Feat/new listitems columns"

--- to_do_list/list/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from list.models import (
-    List,
-    ListItem
-)
-
-admin.site.register(List)
-admin.site.register(ListItem)
--- to_do_list/list/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from list import views
-
-urlpatterns = [
-    path('user_lists/', views.ListViewSet.as_view({'get':'get_user_lists'}), name='get_lists'),
-    path('get_list/<int:list_id>', views.ListViewSet.as_view({'get':'get_user_list','put':'put','delete':'delete'}),name='get user list'),
-    path('items/<int:list_id>', views.ListItemViewSet.as_view({'get':'get_user_list_items'}), name='list items'),
-    path('item/<int:item_id>', views.ListItemViewSet.as_view({'get':'get_item','put':'put','delete':'delete'}),name='get item')
-]
